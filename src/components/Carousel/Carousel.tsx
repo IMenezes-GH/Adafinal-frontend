0dbcdbe4d29196790a87077f2f1fa2b2ae22cd2b
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-11-10 16:19:49 -0300

    Changes how carousel rotation is handled

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -2,8 +2,9 @@ import { Component, MouseEventHandler, MouseEvent } from 'react'
 import styles from './Carousel.module.css'
 
 interface INewsCard {
-    isMiddleCard: boolean,
-    content: string,
+    data: {
+        content: string
+    },
     handleClick: MouseEventHandler<HTMLButtonElement>
 }
 
@@ -11,27 +12,27 @@ export const NewsCard = (props: INewsCard) => {
 
     return (
       <article className={styles.card}>
-        {props.isMiddleCard ? (
+   
         <div>
-          <button id='leftArrow' type='button' value={'subtract'} onClick={(ev) => props.handleClick(ev)}>⮜</button>
-          <span>{props.content}</span>
-          <button id='RightArrow' type='button' value={'add'} onClick={(ev) => props.handleClick(ev)}>⮞</button>
-        </div>):
-        <div>
-          <span></span>
-          <span>{props.content}</span>
-          <span></span>
-        </div>}
+          <button id='leftArrow' type='button' value='subtract' onClick={(ev) => props.handleClick(ev)}>⮜</button>
+          <span>{props.data.content}</span>
+          <button id='RightArrow' type='button' value='add' onClick={(ev) => props.handleClick(ev)}>⮞</button>
+        </div>
       </article>
     )
 }
 
 export default class Carousel extends Component {
   
-    news = ['first', 'second', 'third', 'fourth', 'fifth']
+    // news = 
     currentIndex = 0;
     state = {
-        cards: [this.news[0], this.news[1], this.news[2]],
+        cards: [
+            {content: 'first'}, 
+            {content: 'second'}, 
+            {content: 'third'}, 
+            {content: 'fourth'}, 
+            {content: 'fifth'}],
         currentIndex: 0
     };
 
@@ -39,50 +40,18 @@ export default class Carousel extends Component {
     handleClick(ev: MouseEvent<HTMLButtonElement>){
 
         const target = ev.target as HTMLButtonElement;
-        const operation = target.value;
-
-        if (operation === 'add'){
-            this.currentIndex += 1;
-        }
-        if (operation === 'subtract'){
-            if (this.currentIndex - 1 < 0) this.currentIndex = this.news.length - 1;
-            else this.currentIndex -= 1;
-        }
-
-        if (this.currentIndex + 2 === this.news.length){
-            return this.setState({cards: [
-                this.news[this.currentIndex],
-                this.news[this.currentIndex + 1],
-                this.news[0]
-            ]})
-        }
-        else if (this.currentIndex + 1 === this.news.length){
-            return this.setState({cards: [
-                this.news[this.currentIndex],
-                this.news[0],
-                this.news[1]
-            ]})
-        }
-        else if (this.currentIndex === this.news.length){
-            this.currentIndex = 0;
-        }
-
-        this.setState({cards: [
-            this.news[this.currentIndex],
-            this.news[this.currentIndex + 1],
-            this.news[this.currentIndex + 2]
-        ]})
+        target.value === 'add' ?  this.setState({currentIndex: this.state.currentIndex+= 1}) :this.setState({currentIndex: this.state.currentIndex-= 1})
     
     }
 
 
     render() {
     return (
-    <div className={styles.carousel}>
-        <NewsCard content={this.state.cards[0]} isMiddleCard={false} handleClick={this.handleClick.bind(this)}/>
-        <NewsCard content={this.state.cards[1]} isMiddleCard={true} handleClick={this.handleClick.bind(this)}/>
-        <NewsCard content={this.state.cards[2]} isMiddleCard={false} handleClick={this.handleClick.bind(this)}/>
-      </div>
+    <div id='carousel' className={styles.carousel} style={{transform: `translateX(${-50 * this.state.currentIndex}rem)` ,gridTemplateColumns: `repeat(${this.state.cards.length}, 50rem)`}}>
+        {this.state.cards.map((card, index) => {
+        return <NewsCard key={index} data={card} handleClick={this.handleClick.bind(this)}/>
+        })}
+    </div>
     )
   }
 }
