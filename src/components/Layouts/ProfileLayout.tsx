c7af59dd10538abef6e4d315e67e620678c19918
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-11-12 17:21:19 -0300

    stylizes profile page

--- src/components/Layouts/ProfileLayout.tsx
+++ src/components/Layouts/ProfileLayout.tsx
@@ -1,3 +1,5 @@
+import styles from './ProfileLayout.module.css';
+
 interface props {
   user: User,
   setUser : CallableFunction
@@ -6,9 +8,42 @@ interface props {
 const ProfileLayout = (props: props) => {
 
   return (
-    <main>
-        <p>{props.user.username}
-        </p>
+    <main className={styles.profileBento}>
+      <section>
+        <div className={styles.leftContainer}>
+          <div>
+            <div className={styles.profilePicture}>
+              {/* <img src="./" alt="No profile" /> */}
+            </div>
+            <article className={styles.profileArticle}>
+              <header><span>{props.user.active ? '⬤' : '◯'}</span><span>{props.user.username}</span></header>
+              <div className={styles.profileContent}>
+                <p>{props.user.country}</p>
+                <p>{props.user.name}</p>
+                <p>{props.user.email}</p>
+              </div>
+            </article>
+            <hr />
+            <button className={styles.editProfile}>Editar Perfil</button>
+          </div>
+          <div>
+            <p>Últimos reviews:</p>
+            <ul className={styles.profileReviewsList}>
+              <li>
+                1
+              </li>
+              <li>
+                2
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+        </div>
+        <div className={styles.rightContainer}>
+
+        </div>
+      </section>
     </main>
   )
 }
