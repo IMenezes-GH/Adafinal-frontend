5d3d8c8a37bc8d84a21c82d3b1feb667904156db
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-11-15 18:55:09 -0300

    updates BASE_URL

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://adafinal-backend.vercel.app'
 
 
 export const requestAPI = async(slug: string, options?: RequestInit) => {
